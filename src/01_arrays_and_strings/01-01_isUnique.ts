8183c6d02ec10f582326061b2ae34063d27876cb
Manny Martinez <vccengineers12@gmail.com>
2018-07-29 21:53:34 -0700

    added 1.1

---
+++ src/01_arrays_and_strings/01-01_isUnique.ts
@@ -0,0 +1,33 @@
+function isUnique(str: string): boolean {
+    let map = new Set<string>();
+
+    for (let c of str) {
+        if (!map.has(c)) map.add(c);
+        else return false;
+    }
+
+    return true;
+}
+
+function isUnique_no_ds(str: string): boolean {
+    for (let i = 0; i < str.length; i++) {
+        for (let j = str.length - 1; j > i; j--) {
+            if (str.charAt(j) === str.charAt(i)) return false;
+        }
+    }
+
+    return true;
+}
+
+function isUnique_no_ds2(str: string): boolean {
+    let charSet: boolean[] = [];
+
+    for (let c of str) {
+        if (charSet[c.charCodeAt(0)]) return false;
+        else charSet[c.charCodeAt(0)] = true;
+    }
+
+    return true;
+}
+
+console.log(isUnique_no_ds2("assdf"));
