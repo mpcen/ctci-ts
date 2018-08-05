7006f232ed82b4e3c984a5d793fdd6aad14acfc1
Manny Martinez <vccengineers12@gmail.com>
2018-08-04 20:44:49 -0700

    Added linkedlist class with a ton of methods

--- src/02_linked_lists/LinkedList.ts
+++ src/02_linked_lists/LinkedList.ts
@@ -1,98 +1,197 @@
-interface ILinkedListNode<T> {
-    value: T;
-    next?: ILinkedListNode<T>;
+interface INode<T> {
+    data: T;
+    next: INode<T> | undefined;
 }
 
-class LinkedListNode<T> {
-    public value: T;
-    public next?: ILinkedListNode<T>;
+class Node<T> {
+    public data: T;
+    public next: INode<T> | undefined;
 
-    constructor(value: T) {
-        this.value = value;
+    public constructor(data: T, next?: INode<T>) {
+        this.data = data;
+        this.next = next;
     }
 }
 
 class LinkedList<T> {
-    private head?: ILinkedListNode<T> = undefined;
-    private tail?: ILinkedListNode<T> = undefined;
+    private head?: INode<T>;
 
-    public addToTail(value: T): void {
-        const node: ILinkedListNode<T> = new LinkedListNode(value);
+    public insertFirst(data: T): void {
+        const node: INode<T> = new Node(data);
 
-        if (!this.head) {
-            this.head = node;
-        }
-
-        if (this.tail) {
-            this.tail.next = node;
-        }
-
-        this.tail = node;
+        if (this.head) node.next = this.head;
+        this.head = node;
     }
 
-    public addToHead(value: T): void {
-        const node: ILinkedListNode<T> = new LinkedListNode(value);
+    public size(): number {
+        let currentNode: INode<T> | undefined = this.head;
+        let counter: number = 0;
 
+        while (currentNode) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return counter;
+    }
+
+    public getFirst(): INode<T> | undefined {
+        return this.head;
+    }
+
+    public getLast(): INode<T> | undefined {
+        let currentNode: INode<T> | undefined = this.head;
+
+        while (currentNode) {
+            if (!currentNode.next) return currentNode;
+            currentNode = currentNode.next;
+        }
+
+        return currentNode;
+    }
+
+    public clear(): void {
+        this.head = undefined;
+    }
+
+    public removeFirst(): void {
         if (this.head) {
+            this.head = this.head.next;
+        }
+    }
+
+    public removeLast(): void | undefined {
+        let currentNode: INode<T> | undefined = this.head;
+        let lastNode: INode<T> | undefined = undefined;
+
+        if (!this.head) return undefined;
+        if (!this.head.next) return (this.head = undefined);
+
+        while (currentNode) {
+            if (!currentNode.next) {
+                lastNode!.next = undefined;
+            }
+
+            lastNode = currentNode;
+            currentNode = currentNode.next;
+        }
+    }
+
+    public insertLast(data: T): void | undefined {
+        const node: INode<T> = new Node<T>(data);
+
+        let currentNode: INode<T> | undefined = this.head;
+
+        if (currentNode) {
+            while (currentNode.next) {
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = node;
+            return;
+        }
+
+        this.head = node;
+    }
+
+    public getAt(index: number): INode<T> | undefined {
+        let counter: number = 0;
+        let currentNode: INode<T> | undefined = this.head;
+
+        while (currentNode) {
+            if (counter === index) {
+                return currentNode;
+            }
+
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return undefined;
+    }
+
+    public removeAt(index: number): void {
+        let currentNode: INode<T> | undefined = this.head;
+        let lastNode: INode<T> | undefined = undefined;
+        let counter: number = 0;
+
+        if (!currentNode) return;
+        if (index === 0) {
+            this.head = currentNode.next;
+            return;
+        }
+
+        while (currentNode) {
+            if (counter === index) {
+                lastNode!.next = currentNode.next;
+                return;
+            }
+
+            lastNode = currentNode;
+            currentNode = currentNode.next;
+            counter++;
+        }
+    }
+
+    public insertAt(data: T, index: number): void {
+        let currentNode: INode<T> | undefined = this.head;
+        let lastNode: INode<T> | undefined = undefined;
+        let counter: number = 0;
+        const node: INode<T> = new Node(data);
+
+        if (!currentNode) {
+            this.head = node;
+            return;
+        }
+
+        if (index === 0) {
             node.next = this.head;
             this.head = node;
-        } else {
-            this.head = node;
-            this.tail = node;
+            return;
+        }
+
+        while (currentNode) {
+            if (counter === index) {
+                lastNode!.next = node;
+                node.next = currentNode;
+                return;
+            }
+
+            lastNode = currentNode;
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        lastNode!.next = node;
+    }
+
+    public forEach(cb: (node: INode<T>) => void): void {
+        let currentNode: INode<T> | undefined = this.head;
+
+        if (!currentNode) return;
+
+        while (currentNode) {
+            cb(currentNode);
+            currentNode = currentNode.next;
         }
     }
 
-    public removeHead(): T | null {
-        if (this.head) {
-            const value: T = this.head.value;
-
-            this.head = this.head.next;
-
-            if (!this.head) this.tail = undefined;
-
-            return value;
-        }
-
-        return null;
-    }
-
-    public *values(): IterableIterator<T> {
-        let current = this.head;
-
-        while (current) {
-            yield current.value;
-            current = current.next;
+    *[Symbol.iterator]() {
+        let currentNode: INode<T> | undefined = this.head;
+        while (currentNode) {
+            yield currentNode;
+            currentNode = currentNode.next;
         }
     }
 }
 
 const ll = new LinkedList<number>();
-[1, 2, 3, 4, 5].forEach(n => ll.addToHead(n));
+ll.insertLast(1);
+ll.insertLast(2);
+ll.insertLast(3);
 
-console.log("head");
-for (let node of ll.values()) {
+for (let node of ll) {
     console.log(node);
 }
-console.log("tail");
-console.log("------");
 
-ll.removeHead();
-
-console.log("head");
-for (let node of ll.values()) {
-    console.log(node);
-}
-console.log("tail");
-console.log("------");
-
-ll.removeHead();
-ll.removeHead();
-ll.removeHead();
-ll.removeHead();
-
-console.log("head");
-for (let node of ll.values()) {
-    console.log(node);
-}
-console.log("tail");
-console.log("------");
+export {};
