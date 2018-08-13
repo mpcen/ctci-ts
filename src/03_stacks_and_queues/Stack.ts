ac9db3f8f203c96c219f4fe5ada4b7c03847009c
Manny Martinez <vccengineers12@gmail.com>
2018-08-12 21:05:27 -0700

    added stack class

---
+++ src/03_stacks_and_queues/Stack.ts
@@ -0,0 +1,32 @@
+class Stack<T> {
+    private data: T[] = [];
+
+    public push(item: T): void {
+        this.data.push(item);
+    }
+
+    public pop(): T | undefined {
+        return this.data.pop();
+    }
+
+    public size(): number {
+        if (this.data.length > 0) return this.data.length;
+        return 0;
+    }
+
+    public peek(): T | undefined {
+        return this.data[this.size() - 1];
+    }
+}
+
+const stack = new Stack();
+stack.push("a");
+console.log(stack.size());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.size());
+stack.push("b");
+stack.push("c");
+console.log(stack.peek());
+console.log(stack.size());
