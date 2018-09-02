3e74d0213b40f22c5a49d17e866d74263fe36b85
Manny Martinez <vccengineers12@gmail.com>
2018-09-02 12:24:18 -0700

    added binary tree DFS

---
+++ src/04_trees_and_graphs/binaryTreeDFS.ts
@@ -0,0 +1,28 @@
+class TreeNode<T> {
+    public data: T;
+    public left: TreeNode<T> | undefined = undefined;
+    public right: TreeNode<T> | undefined = undefined;
+
+    public constructor(data: T) {
+        this.data = data;
+    }
+}
+
+const treeNode = new TreeNode(4);
+treeNode.left = new TreeNode(5);
+treeNode.left.left = new TreeNode(8);
+treeNode.left.right = new TreeNode(6);
+treeNode.right = new TreeNode(7);
+treeNode.right.right = new TreeNode(9);
+treeNode.right.left = new TreeNode(2);
+
+function traverseDF<T>(treeNode: TreeNode<T>): void {
+    if (treeNode) {
+        console.log(treeNode.data);
+
+        traverseDF(treeNode.left!);
+        traverseDF(treeNode.right!);
+    }
+}
+
+traverseDF(treeNode);
