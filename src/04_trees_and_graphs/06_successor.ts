6b30771b6b51a4daad23457f5c6717c4e4b7113b
Manny Martinez <vccengineers12@gmail.com>
2018-09-02 22:17:00 -0700

    added 06 node successor

---
+++ src/04_trees_and_graphs/06_successor.ts
@@ -0,0 +1,63 @@
+// Write an algorithm to find the "next" node (i .e ., in-order successor ) of a given node in a
+// binary search tree. You may assume that each node has a link to its parent.
+
+class Node {
+    public data: number;
+    public left: Node | undefined = undefined;
+    public right: Node | undefined = undefined;
+
+    public constructor(data: number) {
+        this.data = data;
+    }
+}
+
+function getAncestorSuccessor(
+    currentNode: Node | undefined,
+    targetNode: Node | undefined,
+    ancestorSuccessorNode: Node | undefined = undefined
+): Node | undefined {
+    if (targetNode === currentNode) return ancestorSuccessorNode;
+
+    if (targetNode!.data <= currentNode!.data) {
+        return getAncestorSuccessor(currentNode!.left, targetNode, currentNode);
+    } else if (targetNode!.data > currentNode!.data) {
+        return getAncestorSuccessor(
+            currentNode!.right,
+            targetNode,
+            ancestorSuccessorNode
+        );
+    }
+
+    return undefined;
+}
+
+function successor(
+    currentNode: Node | undefined,
+    targetNode: Node | undefined
+): Node | undefined {
+    if (!currentNode || !targetNode) return undefined;
+
+    if (targetNode.right) {
+        targetNode = targetNode.right;
+
+        while (targetNode!.left) {
+            targetNode = targetNode!.left;
+        }
+
+        return targetNode;
+    } else {
+        return getAncestorSuccessor(currentNode, targetNode);
+    }
+}
+
+const node = new Node(4);
+node.left = new Node(3);
+node.left.left = new Node(-2);
+node.left.left.right = new Node(2);
+node.right = new Node(9);
+node.right.left = new Node(6);
+node.right.left.left = new Node(5);
+
+console.log(successor(node, node.left)!.data);
+
+export {};
