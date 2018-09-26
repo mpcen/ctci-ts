8e1a83836a87bf3690663cf27056385a87c2eae2
Manny Martinez <vccengineers12@gmail.com>
2018-09-25 22:03:46 -0700

    Added robot in a grid recursive solution

---
+++ src/08_recursion_and_dp/02_robot_in_grid.ts
@@ -0,0 +1,68 @@
+type Grid = (string | number)[][];
+
+const grid: Grid = [
+    [0, "x", "x", "x"],
+    [0, 0, 0, 0],
+    [0, 0, "x", "x"],
+    ["x", 0, 0, 0]
+];
+/*
+    Should result to:
+        [
+            [1,  'x', 'x', 'x'],
+            [1,   1,   0,   0 ],
+            [0,   1,  'x', 'x'],
+            ['x', 1,   1,   1 ]
+        ];
+            Where the starting position is startingGrid[0][0]
+            and ending position is [length-1][length-1]
+*/
+
+function escapeGrid(
+    grid: Grid,
+    row: number = 0,
+    col: number = 0
+): Grid | boolean {
+    // case 0: arrived at end-point
+    if (row === grid.length - 1 && col === grid[0].length - 1) {
+        grid[row][col] = 1;
+
+        console.log("Completed", grid);
+
+        return grid;
+    }
+
+    // case 1: out-of-bounds
+    else if (
+        row < 0 ||
+        row >= grid.length ||
+        col < 0 ||
+        col >= grid[0].length
+    ) {
+        return false;
+    }
+
+    // case 2: in an already-visited space
+    else if (grid[row][col] === 1) {
+        return false;
+    }
+
+    // case 3: in a wall
+    else if (grid[row][col] === "x") {
+        return false;
+    } else {
+        // choose
+        grid[row][col] = 1;
+
+        // explore
+        if (escapeGrid(grid, row, col + 1) || escapeGrid(grid, row + 1, col)) {
+            return true;
+        } else {
+            // unchoose
+            grid[row][col] = 0;
+            return false;
+        }
+    }
+}
+
+escapeGrid(grid);
