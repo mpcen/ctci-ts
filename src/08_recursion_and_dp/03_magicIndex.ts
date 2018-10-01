e2d07fd07035f1a20b2e5cb1919053b1dd53b9fc
Manny Martinez <vccengineers12@gmail.com>
2018-09-30 18:21:51 -0700

    added solution for magic index

--- src/08_recursion_and_dp/03_magicIndex.ts
+++ src/08_recursion_and_dp/03_magicIndex.ts
@@ -1,3 +1,9 @@
+/*
+Magic Index: A magic index in an array A [e ... n -1] is defined to be an index such that A[ i] =
+i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
+array A.
+*/
+
 function magicIndex(
     arr: number[],
     start: number = 0,
@@ -15,3 +21,10 @@ function magicIndex(
 const arr = [-40,-20,-1,1,2,3,5,7,9,12,13];
 
 console.log(magicIndex(arr));
+
+/*
+FOLLOW UP
+What if the values are not distinct?
+*/
+
+// the algorithm above works for both.
