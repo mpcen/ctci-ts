4b83a3a808ef62c4fdcabdbb466e8fbd7d904043
Manny Martinez <vccengineers12@gmail.com>
2018-10-04 22:18:09 -0700

    added 07, permutations without dups

---
+++ src/08_recursion_and_dp/07_permutations_without_dups.ts
@@ -0,0 +1,23 @@
+// Permutations without Dups: Write a method to compute all permutations of a string of unique
+// characters
+
+function permutationsWithoutDups(strArr: string[], chosen: string[] = []) {
+    if(!strArr.length) {
+        console.log(chosen);
+    } else {
+        for(let i = 0; i < strArr.length; i++) {
+            // choose
+            let char = strArr.splice(i, 1)[0];
+            chosen.push(char!);
+
+            // explore
+            permutationsWithoutDups(strArr, chosen);
+
+            // unchoose
+            strArr.splice(i, 0, char);
+            chosen.pop();
+        }
+    }
+}
+
+console.log(permutationsWithoutDups(['a', 's', 'd', 'f']));
